252ec1837122e158db36e4ed093e562109f4eb48
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-14 10:27:46 +0100

    Add solution - CodeHub

---
+++ 7-kyu/isograms/isograms.js
@@ -0,0 +1,12 @@
+function isIsogram(str){
+  arr = str.toLowerCase().split("");
+  
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
