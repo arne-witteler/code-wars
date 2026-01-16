8d82e27b3f3337872ba8c1f77563b65d40be9cb4
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-16 09:11:20 +0100

    Update solution - CodeHub

--- 7-kyu/friend-or-foe/friend-or-foe.js
+++ 7-kyu/friend-or-foe/friend-or-foe.js
@@ -1,10 +1,3 @@
 function friend(friends){
-  let realfriends = [];
-  
-  for (let i=0; i < friends.length; i++) {
-    if (friends[i].length == 4) {
-      realfriends.push(friends[i]);
-    }
-  }
-  return realfriends;
+  return friends.filter(n => n.length === 4)
 }
