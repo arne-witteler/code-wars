43abaa81387d38013d01a24d22f20e81ba253bc1
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-19 15:12:41 +0100

    Add solution - CodeHub

---
+++ 6-kyu/detect-pangram/detect-pangram.js
@@ -0,0 +1,5 @@
+function isPangram(string){
+  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  
+  return alphabet.every(char => string.toLowerCase().includes(char));
+}
