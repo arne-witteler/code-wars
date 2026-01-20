bea25aff08ae3df3f03d537e122f2493d3925747
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-20 09:54:03 +0100

    Add solution - CodeHub

---
+++ 8-kyu/the-feast-of-many-beasts/the-feast-of-many-beasts.js
@@ -0,0 +1,6 @@
+function feast(beast, dish) {
+  if (beast.charAt(0) == dish.charAt(0) && beast.charAt(beast.length - 1) == dish.charAt(dish.length - 1)) {
+    return true;
+  } else
+    return false;
+}
