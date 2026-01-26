72ffd566adcbb0db97b0632edd4e9431b43608d9
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-26 18:09:14 +0100

    Update solution - CodeHub

--- 8-kyu/array-plus-array/array-plus-array.js
+++ 8-kyu/array-plus-array/array-plus-array.js
@@ -1,13 +1,3 @@
 function arrayPlusArray(arr1, arr2) {
-  let sum = 0;
-​
-  for (let num of arr1) {
-    sum += num;
-  }
-​
-  for (let num of arr2) {
-    sum += num;
-  }
-​
-  return sum;
+  return arr1.concat(arr2).reduce((acc, cur) => acc + cur);
 }
