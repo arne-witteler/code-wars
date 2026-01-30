f9fc30279020b67e2a271c7385dde44e906ee3bb
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-01-30 23:12:57 +0100

    Update solution - CodeHub

--- 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
+++ 8-kyu/regular-ball-super-ball/regular-ball-super-ball.js
@@ -1,9 +1,3 @@
 var Ball = function(ballType) {
-  this.ballType = ballType || "regular";
+  this.ballType = ballType || 'regular';
 };
-​
-var ball1 = new Ball();
-var ball2 = new Ball("super");
-​
-ball1.ballType; // "regular"
-ball2.ballType; // "super"
