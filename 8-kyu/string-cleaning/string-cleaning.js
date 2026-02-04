c7e72894d8c103ebec916234dc0ec90890732333
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-02-04 11:17:03 +0100

    Update solution - CodeHub

--- 8-kyu/string-cleaning/string-cleaning.js
+++ 8-kyu/string-cleaning/string-cleaning.js
@@ -1,12 +1,3 @@
-function stringClean(s) {
-  const arr = s.split("")
-  
-  const solution = [];
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= "0" && arr[i] <= "9") {
-    } else
-      solution.push(arr[i])
-  }
-  return solution.join("");
+function stringClean(s){
+  return s.replace(/[0-9]/g, '');
 }
