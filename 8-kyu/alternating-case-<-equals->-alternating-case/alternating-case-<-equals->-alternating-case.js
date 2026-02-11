be4f01fc5e8d1435e6973a24d5d42f2a6059d5ab
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-02-11 09:21:04 +0100

    Update solution - CodeHub

--- 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.js
+++ 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.js
@@ -1,14 +1,10 @@
 String.prototype.toAlternatingCase = function () {
-  const strToArr = this.split("")
-  
-  const newArr = [];
-  
-  for (let i = 0; i < strToArr.length; i++) {
-    if (strToArr[i] === strToArr[i].toLowerCase()) {
-      newArr.push(strToArr[i].toUpperCase())
-    } else {
-      newArr.push(strToArr[i].toLowerCase())
-    }
-  }
-  return newArr.join("");
-}
+  return this
+    .split("")
+    .map(char => 
+      char === char.toLowerCase()
+        ? char.toUpperCase()
+        : char.toLowerCase()
+    )
+    .join("");
+};
