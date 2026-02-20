9c7baae66f78a128bf6b0b9746c3c9da3008f53e
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-02-20 10:49:27 +0100

    Add solution - CodeHub

---
+++ 8-kyu/simple-calculator/simple-calculator.js
@@ -0,0 +1,21 @@
+function calculator(a,b,sign) {
+  
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "unknown value";
+  }
+  
+  switch(sign) {
+  case "+":
+    return a + b;
+    break;
+  case "-":
+    return a - b;
+    break;
+  case "*":
+    return a * b;
+  case "/":
+    return a / b;
+  default:
+    return "unknown value";
+  }
+}
