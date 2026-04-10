ba2ea6bbc27c18b2f2c0a5bf59f6bf14dd489fa4
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-04-10 16:02:25 +0200

    Add solution - CodeHub

---
+++ 7-kyu/remove-the-minimum/remove-the-minimum.js
@@ -0,0 +1,12 @@
+function removeSmallest(numbers) {
+  if (numbers.length === 0) {
+    return [];
+  }
+  
+  const minIndex = numbers.indexOf(Math.min(...numbers));
+  
+  return [
+  ...numbers.slice(0, minIndex),
+  ...numbers.slice(minIndex + 1)
+];
+}
