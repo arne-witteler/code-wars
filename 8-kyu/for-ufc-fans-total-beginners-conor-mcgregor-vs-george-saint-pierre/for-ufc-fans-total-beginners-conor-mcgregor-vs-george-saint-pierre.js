420a6ea7892ade23790cd2d2ca19eed32c674dba
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-19 12:32:57 +0200

    Add solution - CodeHub

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,7 @@
+function quote(fighter) {
+  const winner = fighter.toLowerCase();
+  if (winner === "george saint pierre") {
+    return "I am not impressed by your performance."
+  } else if (winner === "conor mcgregor")
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+};
