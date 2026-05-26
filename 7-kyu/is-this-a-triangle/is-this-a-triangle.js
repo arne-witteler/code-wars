3ca97ff6a1e00b760eae1902360c07e4642ed374
Arne Witteler <160526052+arne-witteler@users.noreply.github.com>
2026-05-26 13:48:55 +0200

    Update solution - CodeHub

--- 7-kyu/is-this-a-triangle/is-this-a-triangle.js
+++ 7-kyu/is-this-a-triangle/is-this-a-triangle.js
@@ -1,7 +1,4 @@
-function isTriangle(a, b, c) {
-  if (a + b > c && b + c > a && c + a > b) {
-    return true;
-  } else {
-    return false;
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
 }
-  }
